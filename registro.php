58d84b0f78346232d94ee83ea4308969ca595dee
Francisco González Rodríguez <94405214+Frannqz@users.noreply.github.com>
2023-05-08 12:50:59 -0600

    Añadido de archivos actualizados

--- registro.php
+++ registro.php
@@ -8,7 +8,8 @@ require 'vendor/autoload.php';
 
 try {
   if (isset($_POST['registro']) && isset($_POST['nombre']) && isset($_POST['correo'])) {
-    $nombre = $_POST['nombre'];
+   if(!empty($_POST['nombre']) && !empty($_POST['correo'])){
+        $nombre = $_POST['nombre'];
     $correo = $_POST['correo'];
 
     //CONFIGURACIÓN PARA ENVIAR CORREO
@@ -17,14 +18,14 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'leonardolemus216@gmail.com';
-    $mail->Password = 'gvribwkvejbnzmys';
+    $mail->Username = 'jesus.salinas.le.9@gmail.com';
+    $mail->Password = 'uoikhogtyebvyhar';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port = 465;
 
-    $mail->setFrom('leonardolemus216@gmail.com', 'Contacto de pagina WEB');
+    $mail->setFrom('jesus.salinas.le.9@gmail.com', 'Contacto de pagina WEB');
     $mail->addAddress('20030048@itcelaya.edu.mx');
-    $mail->addAddress('18030154@itcelaya.edu.mx');
+    $mail->addAddress('19031645@itcelaya.edu.mx');
     $mail->addAddress('20030114@itcelaya.edu.mx');
     $mail->addAddress('20030389@itcelaya.edu.mx');
     $mail->isHTML(true);
@@ -35,9 +36,15 @@ try {
 
     window.location.assign("index.php?r=t")
   </script>';
-  } else {
+   }
+   else {
     echo '<script>
             window.location.assign("index.php?r=f")
+          </script>';
+  }
+  }else{
+        echo '<script>
+            window.location.assign("index.php?r=err")
           </script>';
   }
 } catch (\Throwable $th) {
